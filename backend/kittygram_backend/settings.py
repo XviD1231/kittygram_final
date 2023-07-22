61ae72b91e8123154a3e4f294a407736b2a8b534
Сергей Лющин <xvid1231@yandex.ru>
2023-07-22 20:17:03 +0300

    m

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '123')
 
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['https://kittigramme.sytes.net','localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
